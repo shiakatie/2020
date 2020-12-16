afce2663fccc6864511f7beec4e3fce90df4e550
shiakatie <47531209+shiakatie@users.noreply.github.com>
2020-12-16 13:17:29 -0500

    Add files via upload
    first commit

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+$(function() {
+    $( "#draggable1" ).draggable();
+});
+
+$(function() {
+    $( "#draggable2" ).draggable();
+});
+
+$(function() {
+    $( "#draggable3" ).draggable();
+});
+
+$(function() {
+    $( "#draggable4" ).draggable();
+});
+
+$(function() {
+    $( "#draggable5" ).draggable();
+});
+
+$(function() {
+    $( "#draggable6" ).draggable();
+});
+
+$(function() {
+    $( "#draggable7" ).draggable();
+});
+
+$(function() {
+    $( "#draggable8" ).draggable();
+});
+
+$(function() {
+    $( "#draggable9" ).draggable();
+});
+
+$(function() {
+    $( "#draggable10" ).draggable();
+});
+
+          
